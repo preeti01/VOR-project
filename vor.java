5e9650e1029805222e2c05a9d0f4879db03404f8
preeti01 <preeti01@users.noreply.github.com>
2014-12-06 12:08:10 -0800

    Update vor.java

--- vor.java
+++ vor.java
@@ -2,54 +2,65 @@
 package vor;
 
 
-public class VOR {
+public class VOR 
+{
 	private Radio radio;
+        private int required;
 
-	private int required;
-
-	public VOR() {
+	public VOR()
+	{
 		this(new Radio(true));
 	}
 
-	public VOR(Radio radio) {
+	public VOR(Radio radio)
+	{
 		this.radio = radio;
 		required = 0;
 	}
 
-	public void registerRadioListener(RadioListener listener) {
+	public void registerRadioListener(RadioListener listener) 
+	{
 		radio.addListener(listener);
 	}
 
-	public void rotateOBS(int delta) {
+	public void rotateOBS(int delta) 
+	{
 		required = Angle.normalizeAngle(required + delta);
 	}
 
 
-	public int getOBS() {
+	public int getOBS() 
+	{
 		return required;
 	}
 
 
-	public int getCDI() {
+	public int getCDI() 
+	{
 		int intercepted = radio.getRadial();
 		int curve = Angle.curve(required, intercepted);
-		if (curve > 90) {
+		if (curve > 90) 
+		{
 			curve = 180 - curve;
-		} else if (curve < -90) {
+		} 
+		else if (curve < -90) 
+		{
 			curve = -180 - curve;
 		}
 		return Angle.clamp(curve, -10, 10);
 	}
 
 
-	public boolean isSignalGood() {
+	public boolean isSignalGood() 
+	{
 		int intercepted = radio.getRadial();
 		int curve = Angle.curve(desired, intercepted);
 		return (Math.abs(Math.abs(curve) - 90) > 1) && !radio.isOverStation();
 	}
 
 
-	public boolean isGoingTo() {
+	public boolean isGoingTo() 
+	{
 		int intercepted = radio.getRadial();
 		return Math.abs(Angle.curve(desired, intercepted)) > 90;
 	}
