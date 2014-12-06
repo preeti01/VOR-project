876a05aeb4b7c4ebbee551d7b7d4e3cd16a632ce
preeti01 <preeti01@users.noreply.github.com>
2014-12-06 12:10:46 -0800

    Update angle.java

--- angle.java
+++ angle.java
@@ -2,30 +2,36 @@
 package vor;
 
 
-public class Angle {
+public class Angle
+{
 
 
-	public static int normalizeAngle(int angle, int center) {
+	public static int normalizeAngle(int angle, int center) 
+	{
 		return angle - 360 * (int)Math.floor((angle + 180 - center) / 360.0);
 	}
 
 
-	public static int normalizeAngle(int angle) {
+	public static int normalizeAngle(int angle)
+	{
 		return normalizeAngle(angle, 180);
 	}
 
 
-	public static int clamp(int val, int low, int high) {
+	public static int clamp(int val, int low, int high)
+	{
 		return Math.max(low, Math.min(high, val));
 	}
 
 
-	public static int curve(int x, int y) {
+	public static int curve(int x, int y) 
+	{
 		return normalizeAngle(y - x, 0);
 	}
 
 
-	public static int randomInt(int min, int max) {
+	public static int randomInt(int min, int max) 
+	{
 		return min + (int)(Math.random() * (max - min + 1));
 	}
 }
